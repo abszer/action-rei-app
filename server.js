14ac68c41851077d914b82784d3713370d3318a0
robwhite <robert.white3120@gmail.com>
2021-12-22 16:55:32 -0600

    initial commit w/ webpack

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/test', (req, res) => {
+     res.send("This is a test.")
+})
+
+app.listen(PORT, () => {
+     console.log("Listening on port: " + PORT);
+});
