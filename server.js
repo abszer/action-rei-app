9228ca7d07ebee39ae11fcc16b4888adf54fc13a
robwhite <robert.white3120@gmail.com>
2021-12-23 10:02:58 -0600

    testing connnection to database

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const express = require('express');
+const { Pool } = require('pg');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const connectionString = process.env.DATABASE_URL || "postgresql://localhost:5432/flights";
+
+const pool = new Pool({ connectionString });
+
+
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/test', (req, res) => {
-     res.send("This is a test.")
+     pool.query('SELECT * FROM leads LIMIT 10', (err, response) => {
+          console.log(response.rows);
+          response.rows.forEach((row) => {
+               res.json(row);
+          });
+          pool.end();
+     })
 })
 
 app.listen(PORT, () => {
